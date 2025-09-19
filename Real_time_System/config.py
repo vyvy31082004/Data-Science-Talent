eff9866d4729a0f04b0fe92bb205c9cdb12e9d97
vyvy31082004 <phanthaovy31082004@gmail.com>
2025-09-19 19:21:44 +0700

    update

--- Real_time_System/config.py
+++ Real_time_System/config.py
@@ -8,11 +8,6 @@ load_dotenv()
 FIINQUANT_USERNAME = os.getenv('USERNAME1')
 FIINQUANT_PASSWORD = os.getenv('PASSWORD1')
 
-
-# 4. Cấu hình hệ thống file
 CSV_FILE = 'signals.csv' # File để lưu tín hiệu cho dashboard
 LOG_FILE = 'signals.log' # File để ghi log chi tiết
-
-# 5. Danh sách mã cổ phiếu cần theo dõi
-# Có thể thêm/bớt các mã khác, ví dụ: ['FPT', 'MWG', 'VCB', 'ACB', 'HPG', 'SSI', 'VND']
 TICKERS_WATCHLIST = ['FPT', 'MWG', 'VCB', 'ACB', 'HPG', 'SSI', 'VND', 'VNM', 'VIC', 'MSN']
