a8eedca5ee166f1e0ce7ac24e3d361906f2f09cd
vyvy31082004 <phanthaovy31082004@gmail.com>
2025-09-19 19:20:58 +0700

    update

--- Real_time_System/config.py
+++ Real_time_System/config.py
@@ -8,17 +8,6 @@ load_dotenv()
 FIINQUANT_USERNAME = os.getenv('USERNAME1')
 FIINQUANT_PASSWORD = os.getenv('PASSWORD1')
 
-# 2. Cấu hình Email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_APP_PASSWORD') # Sử dụng App Password của Google
-EMAIL_SENDER_PASSWORD = os.getenv('EMAIL_SENDER_PASSWORD')
-EMAIL_RECEIVER = os.getenv('EMAIL_RECEIVER')
-
-# 3. Cấu hình Telegram
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 # 4. Cấu hình hệ thống file
 CSV_FILE = 'signals.csv' # File để lưu tín hiệu cho dashboard
